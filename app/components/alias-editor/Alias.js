55b024e49ecd58c9b018993a26852bf8ce11c299
Joseph Chua <repeatercreeper@gmail.com>
2019-06-08 19:14:25 -0600

    Alias Editor (command list) header markdown complete.

--- app/components/alias-editor/Alias.js
+++ app/components/alias-editor/Alias.js
@@ -16,28 +16,36 @@ const Alias = {
         fs.readFile(filepath, "utf-8", (err, data) => {
             if (err) throw err;
 
-            let temp = [];
+            let temp = [
+                {
+                    name: "",
+                    commands: []
+                }
+            ];
 
             data.split("\n").forEach(function (line) {
                 if (line.substring(0, 1) == ".") {
                     let command = line.substring(1).split(" ")[0],
                         description = line.substring(1).split(" ").slice(1).join(" ");
 
-                    temp.push({
-                        type: "command",
-                        content: [command, description]
-                    });
+                    if (temp.length == 1) {
+                        temp[0].commands.push([command, description]);
+                    } else {
+                        temp[temp.length - 1].commands.push([command, description]);
+                    }
                 } else if (line.substring(0, 1) == "#") {
-                    temp.push({
-                        type: "header",
-                        content: line.substr(1)
-                    });
+                    if (temp.length == 1 && temp[0].name.length == 0) {
+                        temp[0].name = line.substr(1).trim()
+                    } else {
+                        temp.push({
+                            name: line.substr(1).trim(),
+                            commands: []
+                        });
+                    }
                 }
             });
 
             this.commandsListContents = temp;
-
-            console.log(this.commandsListContents);
         });
     },
     mounted: function(){
